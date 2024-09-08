b9268b0ddf0887c6bd4769168871d388241d6976
vivek kumar <104557123+vivek85410@users.noreply.github.com>
2024-09-09 01:36:59 +0530

    Add files via upload

---
+++ find the ASCII value of character.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void ASCII(char c){
+    cout<<"The ASCII value of "<<c<<" is : "<<int(c)<<endl;
+}
+
+int main(){
+    char c;
+    cout<<"enter the character : ";
+    cin>>c;
+    ASCII(c);
+return 0;
+}
